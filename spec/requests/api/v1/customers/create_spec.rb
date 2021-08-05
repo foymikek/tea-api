d0ca238f6eddd962c1583137bd6bbb70d00dd8cb
foymikek <foymikek@gmail.com>
2021-08-05 17:42:04 -0600

    Add create sad paths

--- spec/requests/api/v1/customers/create_spec.rb
+++ spec/requests/api/v1/customers/create_spec.rb
@@ -4,17 +4,18 @@ RSpec.describe 'Subscribe a Customer' do
   describe 'happy paths' do
     it "can create a new subscription for a customer" do
       customer = Customer.create(
-                    first_name: Faker::Name.first_name,
-                    last_name: Faker::Name.last_name,
-                    email: Faker::Internet.email,
-                    address: Faker::Address.full_address
-                  )
+          first_name: Faker::Name.first_name,
+          last_name:  Faker::Name.last_name,
+          email:      Faker::Internet.email,
+          address:    Faker::Address.full_address
+        )
+
       tea      = Tea.create(
-                    title: Faker::Tea.variety,
-                    description: Faker::Coffee.notes,
-                    temperature: Faker::Number.between(from: 75, to: 100),
-                    brew_time: Faker::Number.between(from: 1, to: 5)
-                  )
+          title:       Faker::Tea.variety,
+          description: Faker::Coffee.notes,
+          temperature: Faker::Number.between(from: 75, to: 100),
+          brew_time:   Faker::Number.between(from: 1, to: 5)
+        )
 
       headers = {
         'Content-Type': "application/json",
@@ -50,6 +51,122 @@ RSpec.describe 'Subscribe a Customer' do
       expect(sub[:data][:attributes][:price]).to be_a(Float)
       expect(sub[:data][:attributes][:status]).to eq("active")
       expect(sub[:data][:attributes][:frequency]).to eq(1)
+    end
+  end
+
+  describe 'sad paths/edge cases' do
+    it 'can show error if field is missing' do
+      customer = Customer.create(
+          first_name: Faker::Name.first_name,
+          last_name:  Faker::Name.last_name,
+          email:      Faker::Internet.email,
+          address:    Faker::Address.full_address
+        )
+
+      tea      = Tea.create(
+          title:       Faker::Tea.variety,
+          description: Faker::Coffee.notes,
+          temperature: Faker::Number.between(from: 75, to: 100),
+          brew_time:   Faker::Number.between(from: 1, to: 5)
+        )
+
+      headers = {
+        'Content-Type': "application/json",
+        'Accept': "application/json"
+      }
+
+      body = {
+        "customer_id": customer.id,
+        "tea_id": tea.id,
+        "title": "#{customer.first_name}'s Subscription for #{tea.title}",
+        "price": 12.05,
+        "status": 0
+      }
+
+      post "/api/v1/customers/#{customer.id}/subscriptions", headers: headers, params: body.to_json
+      sub = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+
+      expect(sub[:errors]).to eq("Frequency can't be blank")
+    end
+
+    it 'alerts customer if they are already subscribed to a tea' do
+      customer = Customer.create(
+          first_name: Faker::Name.first_name,
+          last_name:  Faker::Name.last_name,
+          email:      Faker::Internet.email,
+          address:    Faker::Address.full_address
+        )
+
+      tea      = Tea.create(
+          title:       Faker::Tea.variety,
+          description: Faker::Coffee.notes,
+          temperature: Faker::Number.between(from: 75, to: 100),
+          brew_time:   Faker::Number.between(from: 1, to: 5)
+        )
+
+      customer.subscriptions.create(
+          title:     Faker::Lorem.sentence(word_count: 3),
+          price:     Faker::Commerce.price(range: 10.0..30.0),
+          status:    "active",
+          tea_id:    tea.id,
+          frequency: (0..99).to_a.sample
+        )
+
+      headers = {
+        'Content-Type': "application/json",
+        'Accept': "application/json"
+      }
+
+      body = {
+        "customer_id": customer.id,
+        "tea_id": tea.id,
+        "title": "#{customer.first_name}'s Subscription for #{tea.title}",
+        "price": 12.05,
+        "status": 0,
+        "frequency": 0
+      }
+
+      post "/api/v1/customers/#{customer.id}/subscriptions", headers: headers, params: body.to_json
+      sub = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+
+      expect(sub[:errors]).to eq("You're already subscribed to this tea")
+    end
+
+    it 'shows an error if tea does not exist' do
+      customer = Customer.create(
+          first_name: Faker::Name.first_name,
+          last_name:  Faker::Name.last_name,
+          email:      Faker::Internet.email,
+          address:    Faker::Address.full_address
+        )
+
+      headers = {
+        'Content-Type': "application/json",
+        'Accept': "application/json"
+      }
+
+      body = {
+        "customer_id": customer.id,
+        "tea_id": 1,
+        "title": "#{customer.first_name}'s Subscription for a tea that doens't exist",
+        "price": 12.05,
+        "status": 0,
+        "frequency": 0
+      }
+
+      post "/api/v1/customers/#{customer.id}/subscriptions", headers: headers, params: body.to_json
+      sub = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+
+      # expect(sub[:errors]).to eq("Cannot find tea")
     end
   end
 end
--- spec/requests/api/v1/customers/update_spec.rb
+++ spec/requests/api/v1/customers/update_spec.rb
@@ -4,40 +4,43 @@ RSpec.describe 'Update Subscriptions' do
   describe 'happy paths' do
     it "can cancel a customers subscription" do
       customer = Customer.create(
-              first_name: Faker::Name.first_name,
-              last_name: Faker::Name.last_name,
-              email: Faker::Internet.email,
-              address: Faker::Address.full_address
+         first_name: Faker::Name.first_name,
+         last_name:  Faker::Name.last_name,
+         email:      Faker::Internet.email,
+         address:    Faker::Address.full_address
        )
 
       tea      = Tea.create(
-              title: Faker::Tea.variety,
-              description: Faker::Coffee.notes,
-              temperature: Faker::Number.between(from: 75, to: 100),
-              brew_time: Faker::Number.between(from: 1, to: 5)
+          title:       Faker::Tea.variety,
+          description: Faker::Coffee.notes,
+          temperature: Faker::Number.between(from: 75, to: 100),
+          brew_time:   Faker::Number.between(from: 1, to: 5)
         )
             
-      sub_1 = customer.subscriptions.create(
-        title: Faker::Lorem.sentence(word_count: 3),
-        price: Faker::Commerce.price(range: 10.0..30.0),
-        status: "active",
-        tea_id: tea.id,
-        frequency: (0..99).to_a.sample
+      sub_1    = customer.subscriptions.create(
+          title:     Faker::Lorem.sentence(word_count: 3),
+          price:     Faker::Commerce.price(range: 10.0..30.0),
+          status:    "active",
+          tea_id:    tea.id,
+          frequency: (0..99).to_a.sample
         )
-      sub_2 = customer.subscriptions.create(
-        title: Faker::Lorem.sentence(word_count: 3),
-        price: Faker::Commerce.price(range: 10.0..30.0),
-        status: "active",
-        tea_id: tea.id,
-        frequency: (0..99).to_a.sample
-      )
-      sub_3 = customer.subscriptions.create(
-        title: Faker::Lorem.sentence(word_count: 3),
-        price: Faker::Commerce.price(range: 10.0..30.0),
-        status: "active",
-        tea_id: tea.id,
-        frequency: (0..99).to_a.sample
-      )
+
+      sub_2    = customer.subscriptions.create(
+          title:     Faker::Lorem.sentence(word_count: 3),
+          price:     Faker::Commerce.price(range: 10.0..30.0),
+          status:    "active",
+          tea_id:    tea.id,
+          frequency: (0..99).to_a.sample
+        )
+
+      sub_3    = customer.subscriptions.create(
+          title:     Faker::Lorem.sentence(word_count: 3),
+          price:     Faker::Commerce.price(range: 10.0..30.0),
+          status:    "active",
+          tea_id:    tea.id,
+          frequency: (0..99).to_a.sample
+        )
+
       expect(sub_1.status).to eq("active")
       expect(sub_2.status).to eq("active")
       expect(sub_3.status).to eq("active")
